d6ef4d5951a0703d42a5a7a2c8f86780783331d6
Danilo Ribeiro <danriba@gmail.com>
2021-03-17 15:08:35 -0300

    Implements the ML aspects and prediction

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
-from sqlalchemy import create_engine
 import pandas as pd
+import numpy as np
+import pprint
+from sqlalchemy import create_engine
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 
 config = {
   'user': 'root',
@@ -10,34 +17,115 @@ config = {
 }
 
 def main():
+  ####### Translating column names
+  german_english = {'id': 'id'}
+  description = {}
 
-    db_connection_str = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(
-        config['user'],
-        config['password'],
-        config['host'],
-        config['port'],
-        config['database']
-      )
-    
-    codetable = open('documents//codetable.txt', 'rt')
-    lines = codetable.readlines()
-    german_english = {'id': 'id'}
-    
-    for line in lines:
-      if('$' in line.strip()):
-        line_splited = line.strip()[2:-1].split()
-        german_english.update({line_splited[0]: line_splited[2]})
+  codetable = open('documents//codetable.txt', 'rt')
+  lines = codetable.readlines()
 
-    db_connection = create_engine(db_connection_str)
+  for line in lines:
+    if('$' in line.strip()):
+      line_splited = line.strip()[2:-1].split()
+      
+      current_item_de = line_splited[0]
+      current_item_en = line_splited[2]
+      german_english.update({current_item_de: current_item_en})
 
-    column_names = list(german_english.values())
+      description.update({current_item_en: ''})
+    else:
+      description.update({
+        current_item_en: description[current_item_en] + line
+      })
+        
+  #pprint.pprint(description)
 
-    df = pd.read_sql('SELECT * FROM germancredit', con=db_connection)
+  ####### Starting connection with mySQL to getting data
+  db_connection_str = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(
+      config['user'],
+      config['password'],
+      config['host'],
+      config['port'],
+      config['database']
+  )
 
-    df.columns = column_names
+  db_connection = create_engine(db_connection_str)
 
-    print(df)
+  column_names = list(german_english.values())
 
+  df = pd.read_sql('SELECT * FROM germancredit', con=db_connection)
+  df.columns = column_names
+
+  ####### Preparing the data and spliting in to train and test
+  X = df.iloc[:,1:-1].to_numpy() ## Data
+  y = df.iloc[:,-1:].T.to_numpy()[0] ## Target
+
+  test_size = 0.2
+  random_state = 0
+
+  X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=test_size,
+    random_state=random_state
+  )
+
+  # Standardize data
+  sc = StandardScaler()
+  sc.fit(X_train)
+  X_train_std = sc.transform(X_train)
+  X_test_std = sc.transform(X_test)
+
+  print('Unique labels: {0}'.format(np.unique(y)))
+  
+  ####### Training model with data
+  # Creating an instance of Perceptron
+  n_iter = 400
+  eta0 = 0.1
+
+  ppn = Perceptron(
+    max_iter=n_iter,
+    eta0=eta0,
+    random_state=random_state
+  )
+  
+  # fit the model to the standardized data
+  ppn.fit(
+    X_train_std,
+    y_train
+  )
+
+  ####### Making predictions
+  y_pred = ppn.predict(X_test_std)
+
+  ####### Getting measures
+  print(50 * "-")
+  print("accuracy: {0:.2f}%".format(accuracy_score(y_test, y_pred) * 100))
+  print(classification_report(y_test, y_pred))
+  print(50 * "-")
+
+  answers = {}
+
+  print('Instert the values for:')
+
+  for item in column_names[1:-1]:
+    print(50 * "-")
+    print(item + " " + ((49 - len(item)) * '-'))
+    print("description:")
+    print(description[item])
+    print(50 * "-")
+    answers.update({item: input(item + ' choice: ')})
+    print(50 * "-")
+
+  X_user = np.array([list(answers.values())], dtype=np.int64)
+  X_user_std = sc.transform(X_user)
+
+  y_user_pred = ppn.predict(X_user_std)
+
+  if y_user_pred[0] == 0:
+    print("That's profile has a BAD credit risk")
+  else:
+    print("That's profile has a GOOD credit risk")
 
 if __name__ == "__main__":
-    main()
+  main()
